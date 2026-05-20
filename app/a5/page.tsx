d8abd4f74db27758c894a4d5118845aca3eb5901
ImRegActually <vibhavbionapally2@gmail.com>
2026-05-20 13:09:30 -0700

    Add files via upload

---
+++ app/a5/page.tsx
@@ -0,0 +1,66 @@
+import { Image } from "@heroui/image";
+
+import { title } from "@/components/primitives";
+
+export default function A5Page() {
+  return (
+    <div>
+      <h1 className={title()}>A5 Defend Fictional Villian</h1>
+      <p />
+      <Image
+        alt="Joker"
+        src="https://i2.wp.com/suunews.net/wp-content/uploads/2019/10/MV5BZTIwNDNhNjQtNzc0YS00ZTRmLTg3ODctOWM2YzkwNjIxYmU3XkEyXkFqcGdeQXVyNjczOTE0MzM@._V1_SX1777_CR001777999_AL_.jpg?fit=1777%2C999"
+        width={100}
+      />
+      {/* import {TextArea} from "@heroui/react";
+
+    <TextArea
+      aria-label="Quick project update"
+      className="h-32 w-96"
+      placeholder="Share a quick project update..."
+    /> */}
+      <p>
+        Joker was often ignored and treated unfairly by society, which made him
+        feel invisible and unimportant.{" "}
+      </p>
+      <Image
+        alt="Joker"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb3QySUn7s2r7WuHFHSQldJL9-r7BMAVabZg&s"
+        width={100}
+      />
+      <p>
+        He struggled with mental illness and emotional pain, but many people
+        around him did not understand what he was going through.
+      </p>
+      <Image
+        alt="Joker smiling"
+        src="https://hips.hearstapps.com/hmg-prod/images/03-the-joker-w1200-h630-1562679871.jpg?crop=1xw:0.9523809523809523xh;center,top&resize=1200:*"
+        width={100}
+      />
+      <p>
+        {" "}
+        Instead of receiving support or kindness, people judged his strange
+        behavior and pushed him away even more.
+      </p>
+      <Image
+        alt="Joker"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUs4OPOIaLnurcc1Rxu7x7NeKHaLfFH81yrg&s"
+        width={100}
+      />
+      <p>
+        He experienced loneliness and rejection for most of his life, causing
+        him to lose trust in other people
+      </p>
+      <Image
+        alt="Joker smiling"
+        src="https://batman-escape-assets-2.cdn.ooeo.net/ZhVX0DjCgu4jztPh_heath-ledger-joker-incroyable.webp?auto=format,compress&w=800"
+        width={100}
+      />
+      <p>
+        {" "}
+        His traumatic childhood and difficult experiences had a major impact on
+        his personality and the choices he later made.
+      </p>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+// import { TextArea } from '@heroui/react';
+export default function Home() {
+  return (
+    <div>
+      {/* import {TextArea} from "@heroui/react";
+
+    <TextArea
+      aria-label="Quick project update"
+      className="h-32 w-96"
+      placeholder="Share a quick project update..."
+    /> */}
+    </div>
+  );
+}
